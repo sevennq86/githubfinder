c614e9bee18d78464737d75b949ad7e9f835c383
sevennq86 <brandonneves@brandons-mbp.lan>
2023-06-25 10:58:43 -0400

    initial commit

---
+++ src/components/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import UserResults from "../Users/UserResults"
+
+function Home() {
+  return (
+    <>
+      {/* Search Component will go here */}
+      <UserResults />
+    </>
+  )
+}
+
+export default Home
